4edefc3e879c26e8b6ef3b6c10d48aaaee369a2e
Apprentice <apprentice@devbootcamp.com>
2012-11-05 16:46:20 -0800

    (sur)render and redirecting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,10 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @article, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
-        format.html { redirect_to @comment.article }
+        format.html { redirect_to @comment.article, notice: 'Failed to validate comment.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :article
 
   attr_accessible :body
+  validates :body, :presence => true
 end
